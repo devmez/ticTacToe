19ffbe54431d311c3ad916b95216823ecd84d65e
Christian Marc Caballero <mez.codedev@gmail.com>
2019-12-31 22:12:02 -0500

    second commit

--- ticTacToe.js
+++ ticTacToe.js
@@ -13,13 +13,12 @@ function preGameInit(){
         console.log(' '   + `\n` + `===== W E L C O M E   T O   T I C - T A C - T O E =====` + `\n` + ` `  + `\n` +
                     `INSTRUCTIONS:`  + `\n` +
                     `NOTE THE NUMBERS AND THEIR POSITIONS BELOW.`  + `\n` +
-                    `THESE "NUMBERS" WILL BE YOUR [POSITION INPUT] TO SELECT YOUR SPOT.` + `\n` + ` ` + `\n` +
+                    `THESE [NUMBERS] WILL BE YOUR [INPUT] TO SELECT YOUR SPOT.` + `\n` + ` ` + `\n` +
                         ` [1] | [2] | [3] ` + `\n` +
                         `-----|-----|-----`  + `\n` +
                         ` [4] | [5] | [6] ` + `\n` +
                         `-----|-----|-----`  + `\n` +
                         ` [7] | [8] | [9] `  + `\n` + ` `);
-        gridInput();
     };
 };
 preGameInit();
@@ -27,16 +26,43 @@ preGameInit();
 
 function playerOneTurn(){
     let playerOneInput = actionPrompt.question(`P1: `);
-    
-    if ()
 
-    let playerOneInput = playerOne.push(actionPrompt.question(`P1: `));
+    if (playerOneInput === 1){
+        grid[0].push(`X`);
+        checkWin();
+    }else if (playerOneInput === 2){
+        grid[0].push(`X`);
+        checkWin();
+    }else if (playerOneInput === 3){      
+        grid[0].push(`X`);
+        checkWin();  
+    }else if (playerOneInput === 4){   
+        grid[0].push(`X`);
+        checkWin();     
+    }else if (playerOneInput === 5){    
+        grid[0].push(`X`);
+        checkWin();    
+    }else if (playerOneInput === 6){
+        grid[0].push(`X`);
+        checkWin();        
+    }else if (playerOneInput === 7){  
+        grid[0].push(`X`);
+        checkWin();      
+    }else if (playerOneInput === 8){
+        grid[0].push(`X`);
+        checkWin();
+    }else if (playerOneInput === 9){
+        grid[0].push(`X`);
+        checkWin();
+    }else{
+        console.log(`You did not enter a valid input.` + `\n` + `Try again.`)
+    };
 };
 playerOneTurn();
 
-function checkWin(grid){
+// function checkWin(grid){
 
-};
+// };
 
 
 
