1b2f87c34622aa5bc785075b54c540fdba36aeee
Devmez <mez.codedev@gmail.com>
2020-01-01 07:13:00 -0500

    7:12am checkpoint

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,5 +1,6 @@
 let grid = [[`[ ]`, `[ ]`, `[ ]`], [`[ ]`, `[ ]`, `[ ]`], [`[ ]`, `[ ]`, `[ ]`]];
 
+/*
 // ===== Visual display of the grid for [console.log] =====
 let gridVisual =
 ` ` + grid[0][0] + ` ` + `|` + ` ` + grid[0][1] + ` ` + `|`  + ` ` + grid[0][2] + ` ` + `\n` +
@@ -8,11 +9,16 @@ let gridVisual =
 `-----|-----|-----`  + `\n` +
 ` ` + grid[2][0] + ` ` + `|` + ` ` + grid[2][1] + ` ` + `|`  + ` ` + grid[2][2] + ` ` + `\n` + ` `;
 // =========================================================
+*/
+
+let playerOneInput = null;
+let playerTwoInput = null;
 
 let actionPrompt = require(`readline-sync`);
-let playerInput = actionPrompt.question(`This game requires TWO [2] players.` + `\n` + `Type [y] to continue: `);
 
 function preGameInit(){
+    let playerInput = actionPrompt.question(`This game requires TWO [2] players.` + `\n` + `Type [y] to continue: `);
+
     if (playerInput == `y`){
         console.log(' '   + `\n` + `===== W E L C O M E   T O   T I C - T A C - T O E =====` + `\n` + ` `  + `\n` +
                     `INSTRUCTIONS:`  + `\n` +
@@ -23,196 +29,185 @@ function preGameInit(){
                         ` [4] | [5] | [6] ` + `\n` +
                         `-----|-----|-----`  + `\n` +
                         ` [7] | [8] | [9] `  + `\n` + ` `);
+    }else{
+        console.log(`Invalid input.` + `\n` + `Type [y] to continue: `)
+        preGameInit();
     };
 };
 
 function playerOneTurn(){
-    let playerOneInput = actionPrompt.question(`P1: `);
+    playerOneInput = actionPrompt.question(`Player One[1]: `);
 
-    if (playerOneInput == 1){
+    if (playerOneInput == 1 && !playerTwoInput == 1){
         grid[0][0] = `[X]`;
         console.log(grid);
         checkWinP1();
-    }else if (playerOneInput == 2){
+    }
+    if (playerOneInput == 2 && !playerTwoInput == 2){
         grid[0][1] = `[X]`;
+        console.log(grid);
         checkWinP1();
-    }else if (playerOneInput == 3){      
+    }
+    if (playerOneInput == 3 && !playerTwoInput == 3){      
         grid[0][2] = `[X]`;
+        console.log(grid);
         checkWinP1();
-    }else if (playerOneInput == 4){   
+    }
+    if (playerOneInput == 4 && !playerTwoInput == 4){   
         grid[1][0] = `[X]`;
+        console.log(grid);
         checkWinP1();
-    }else if (playerOneInput == 5){    
+    }
+    if (playerOneInput == 5 && !playerTwoInput == 5){    
         grid[1][1] = `[X]`;
+        console.log(grid);
         checkWinP1();
-    }else if (playerOneInput == 6){
+    }
+    if (playerOneInput == 6 && !playerTwoInput == 6){
         grid[1][2] = `[X]`;
+        console.log(grid);
         checkWinP1();
-    }else if (playerOneInput == 7){  
+    }
+    if (playerOneInput == 7 && !playerTwoInput == 7){  
         grid[2][0] = `[X]`;
+        console.log(grid);
         checkWinP1();
-    }else if (playerOneInput == 8){
+    }
+    if (playerOneInput == 8 && !playerTwoInput == 8){
         grid[2][1] = `[X]`;
+        console.log(grid);
         checkWinP1();
-    }else if (playerOneInput == 9){
+    }
+    if (playerOneInput == 9 && !playerTwoInput == 9){
         grid[2][2] = `[X]`;
+        console.log(grid);
         checkWinP1();
     }else{
-        console.log(`You did not enter a valid input.` + `\n` + `Try again.`)
-        playerOneInput = actionPrompt.question(`P1: `);
+        console.log(`Invalid input.` + `\n` + `Try again.`)
+        playerOneTurn();
     };
 };
 
 function playerTwoTurn(){
-    let playerOneInput = actionPrompt.question(`P2: `);
+    playerTwoInput = actionPrompt.question(`Player Two[2]: `);
+    console.log(playerOneInput);
 
-    if (playerOneInput == 1){
+    if (playerTwoInput == 1 && !playerOneInput == 1){
         grid[0][0] = `[O]`;
+        console.log(grid);
         checkWinP2();
-    }else if (playerOneInput == 2){
+    }else if (playerTwoInput == 2 && !playerOneInput == 2){
         grid[0][1] = `[O]`;
+        console.log(grid);
         checkWinP2();
-    }else if (playerOneInput == 3){      
+    }else if (playerTwoInput == 3 && !playerOneInput == 3){      
         grid[0][2] = `[O]`;
+        console.log(grid);
         checkWinP2();
-    }else if (playerOneInput == 4){   
+    }else if (playerTwoInput == 4 && !playerOneInput == 4){   
         grid[1][0] = `[O]`;
+        console.log(grid);
         checkWinP2();
-    }else if (playerOneInput == 5){    
+    }else if (playerTwoInput == 5 && !playerOneInput == 5){    
         grid[1][1] = `[O]`;
+        console.log(grid);
         checkWinP2();
-    }else if (playerOneInput == 6){
+    }else if (playerTwoInput == 6 && !playerOneInput == 6){
         grid[1][2] = `[O]`;
+        console.log(grid);
         checkWinP2();
-    }else if (playerOneInput == 7){  
+    }else if (playerTwoInput == 7 && !playerOneInput == 7){  
         grid[2][0] = `[O]`;
+        console.log(grid);
         checkWinP2();
-    }else if (playerOneInput == 8){
+    }else if (playerTwoInput == 8 && !playerOneInput == 8){
         grid[2][1] = `[O]`;
+        console.log(grid);
         checkWinP2();
-    }else if (playerOneInput == 9){
+    }else if (playerTwoInput == 9 && !playerOneInput == 9){
         grid[2][2] = `[O]`;
+        console.log(grid);
         checkWinP2();
     }else{
-        console.log(`You did not enter a valid input.` + `\n` + `Try again.`)
-        playerTwoInput = actionPrompt.question(`P2: `);
+        console.log(`Invalid input.` + `\n` + `Try again.`)
+        playerTwoTurn();
     };
 };
 
 function checkWinP1(){
-    if (grid[0][0] === `X` && grid[0][1] === `X` && grid[0][2] === `X`){
+    if (grid[0][0] === `[X]` && grid[0][1] === `[X]` && grid[0][2] === `[X]`){
         console.log(`Player One Wins!`);
-    }else if (grid[0][0] === `X` && grid[0][1] === `X` & grid[0][2] === `X`){
+    }else if (grid[0][0] === `[X]` && grid[0][1] === `[X]` && grid[0][2] === `[X]`){
         console.log(`Player One Wins!`);
-    }else if (grid[1][0] === `X` && grid[1][1] === `X` & grid[1][2] === 'X'){
+    }else if (grid[1][0] === `[X]` && grid[1][1] === `[X]` && grid[1][2] === '[X]'){
         console.log(`Player One Wins!`);
-    }else if (grid[2][0] === `X` && grid[2][1] === `X` & grid[2][2] === `X`){
+    }else if (grid[2][0] === `[X]` && grid[2][1] === `[X]` && grid[2][2] === `[X]`){
         console.log(`Player One Wins!`);
-    }else if (grid[0][0] === `X` && grid[1][0] === `X` & grid[2][0] === `X`){
+    }else if (grid[0][0] === `[X]` && grid[1][0] === `[X]` && grid[2][0] === `[X]`){
         console.log(`Player One Wins!`);
-    }else if (grid[0][1] === `X` && grid[1][1] === `X` & grid[2][1] === `X`){
+    }else if (grid[0][1] === `[X]` && grid[1][1] === `[X]` && grid[2][1] === `[X]`){
         console.log(`Player One Wins!`);
-    }else if (grid[0][2] === `X` && grid[1][2] === `X` & grid[2][2] === `X`){
+    }else if (grid[0][2] === `[X]` && grid[1][2] === `[X]` && grid[2][2] === `[X]`){
         console.log(`Player One Wins!`);
-    }else if (grid[2][0] === `X` && grid[1][1] === `X` & grid[0][2] === `X`){
+    }else if (grid[2][0] === `[X]` && grid[1][1] === `[X]` && grid[0][2] === `[X]`){
         console.log(`Player One Wins!`);
-    }else if (grid[0][0] === `X` && grid[1][1] === `X` & grid[2][2] === `X`){
+    }else if (grid[0][0] === `[X]` && grid[1][1] === `[X]` && grid[2][2] === `[X]`){
         console.log(`Player One Wins!`);
     }else if (
-        grid[0][0] === `` ||
-        grid[0][1] === `` ||
-        grid[0][2] === `` ||
-        grid[1][0] === `` ||
-        grid[1][2] === `` ||
-        grid[1][2] === `` ||
-        grid[2][0] === `` ||
-        grid[2][1] === `` ||
-        grid[2][2] === ``
+        grid[0][0] == `[ ]` ||
+        grid[0][1] == `[ ]` ||
+        grid[0][2] == `[ ]` ||
+        grid[1][0] == `[ ]` ||
+        grid[1][2] == `[ ]` ||
+        grid[1][2] == `[ ]` ||
+        grid[2][0] == `[ ]` ||
+        grid[2][1] == `[ ]` ||
+        grid[2][2] == `[ ]`
     ){
         playerTwoTurn();
     }else{
         console.log(`TIE GAME!`);
-        playAgain();
+        //playAgain();
     }
 };
 
 function checkWinP2(){
-    if (grid[0][0] === `O` && grid[0][1] === `O` & grid[0][2] === `O`){
+    if (grid[0][0] === `[O]` && grid[0][1] === `[O]` && grid[0][2] === `[O]`){
         console.log(`Player Two Wins!`);
-    }else if (grid[0][0] === `O` && grid[0][1] === `O` & grid[0][2] === `O`){
+    }else if (grid[0][0] === `[O]` && grid[0][1] === `[O]` && grid[0][2] === `[O]`){
         console.log(`Player Two Wins!`);
-    }else if (grid[1][0] === `O` && grid[1][1] === `O` & grid[1][2] === `O`){
+    }else if (grid[1][0] === `[O]` && grid[1][1] === `[O]` && grid[1][2] === '[O]'){
         console.log(`Player Two Wins!`);
-    }else if (grid[2][0] === `O` && grid[2][1] === `O` & grid[2][2] === `O`){
+    }else if (grid[2][0] === `[O]` && grid[2][1] === `[O]` && grid[2][2] === `[O]`){
         console.log(`Player Two Wins!`);
-    }else if (grid[0][0] === `O` && grid[1][0] === `O` & grid[2][0] === `O`){
+    }else if (grid[0][0] === `[O]` && grid[1][0] === `[O]` && grid[2][0] === `[O]`){
         console.log(`Player Two Wins!`);
-    }else if (grid[0][1] === `O` && grid[1][1] === `O` & grid[2][1] === `O`){
+    }else if (grid[0][1] === `[O]` && grid[1][1] === `[O]` && grid[2][1] === `[O]`){
         console.log(`Player Two Wins!`);
-    }else if (grid[0][2] === `O` && grid[1][2] === `O` & grid[2][2] === `O`){
+    }else if (grid[0][2] === `[O]` && grid[1][2] === `[O]` && grid[2][2] === `[O]`){
         console.log(`Player Two Wins!`);
-    }else if (grid[2][0] === `O` && grid[1][1] === `O` & grid[0][2] === `O`){
+    }else if (grid[2][0] === `[O]` && grid[1][1] === `[O]` && grid[0][2] === `[O]`){
         console.log(`Player Two Wins!`);
-    }else if (grid[0][0] === `O` && [grid[1][1]] === `O` & grid[2][2] === `O`){
+    }else if (grid[0][0] === `[O]` && grid[1][1] === `[O]` && grid[2][2] === `[O]`){
         console.log(`Player Two Wins!`);
     }else if (
-        grid[0][0] === `` ||
-        grid[0][1] === `` ||
-        grid[0][2] === `` ||
-        grid[1][0] === `` ||
-        grid[1][2] === `` ||
-        grid[1][2] === `` ||
-        grid[2][0] === `` ||
-        grid[2][1] === `` ||
-        grid[2][2] === ``
+        grid[0][0] == `[ ]` ||
+        grid[0][1] == `[ ]` ||
+        grid[0][2] == `[ ]` ||
+        grid[1][0] == `[ ]` ||
+        grid[1][2] == `[ ]` ||
+        grid[1][2] == `[ ]` ||
+        grid[2][0] == `[ ]` ||
+        grid[2][1] == `[ ]` ||
+        grid[2][2] == `[ ]`
     ){
         playerOneTurn();
     }else{
         console.log(`TIE GAME!`);
-        playAgain();
+        //playAgain();
     }
 };
-
-
 
 //GAME INIT
 preGameInit();
 playerOneTurn();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function gridInput(){
-//     let playerOneInput = playerOne.push(actionPrompt.question(`P1: `));
-//     let playerTwoInput = playerTwo.push(actionPrompt.question(`P2: `));
-// };
-
-// function winCombo(){
-//     let playerOne = [];
-//     let playerTwo = [];
-
-//     if ()
-// };
-
-// function gameInit(){
-//     let playerOne = [];
-//     let playerTwo = [];
-//     let winCombo = {
-        
-//     };
-
-//     let playerOneInput = playerOne.push(actionPrompt.question(`P1: `));
-//     let playerTwoInput = playerTwo.push(actionPrompt.question(`P2: `));
-
-//     if (playerOne == )
-
-// };
